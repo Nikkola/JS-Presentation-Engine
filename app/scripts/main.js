404ccad753b4fd02f82a5a6543a5dbb2538eb8be
Nicolay Chernobaev <nicolachernobaev@gmail.com>
2013-02-20 20:13:58 +0300

    create and render SlidesViewCollection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,3 +14,6 @@ require.config({
 });
  
 
+require(['views/app'], function(App) {
+	new App();
+});
--- app/scripts/views/slide.js
+++ app/scripts/views/slide.js
@@ -6,6 +6,8 @@ define(['backbone'], function(Backbone) {
 			this.$el.append(
 				'<h1>' + this.model.get('title') + '</h1>'
 			);	
+
+			return this;
 		}
 	});
 
