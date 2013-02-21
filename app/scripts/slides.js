750976016b8b60cf2b1d129e31e18f0985135fc3
Nicolay Chernobaev <nicolachernobaev@gmail.com>
2013-02-21 22:41:48 +0300

    add image content

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -1,7 +1,7 @@
 window.slides = [
 	{ title: 'Моя презентация' },
+	{ image: 'http://placekitten.com/600/300' },
 	{ title: 'Привет, меня зовут Николай' },
-	{ title: 'Я веб-разработчик sdgfsgsgdsgsdgsdsdgsdgsdgsdgsgsdgsdgsdgsdgsdggggggggggggggggggggggggggggggggggggggggggg' },
-	{ title: 'Я веб-разработчик    sdgfsgsgdsgsdgsdsdgsdgsdgsdgsgsdgsdgsdgsdgsdggggggggg' }
+	{ title: 'Я веб-разработчик'}
 
 ];
